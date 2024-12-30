1af4ed02ce88786c6ade2f02085ed9cc4df2b3d4
Vijay Kumar R <115397037+vijaykumarr1452@users.noreply.github.com>
2024-12-30 13:51:35 +0530

    Updated bug @navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ const navItems = [
   { name: "Contact", path: "#contact" },
 ]
 
-export unction Navbar() {
+export function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false)
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const [mounted, setMounted] = useState(false)
